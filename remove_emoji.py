68f84ee5685ebd070da86149d1a0ddd880d95a41
Shashank S <40292770+shashanks33@users.noreply.github.com>
2024-04-13 15:36:33 +0530

    Added a new code snippet - Emoji Removal
    The code helps in removing emoji's from text files and returns a new text file with no emojis.

---
+++ remove_emoji.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+Remove emoji from a text file and print it to stdout.
+Usage
+-----
+    python remove_emoji.py input.txt > output.txt
+"""
+import re
+import sys
+
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+if __name__ == '__main__':
+    text = open(sys.argv[1], encoding='utf8').read()
+    text = remove_emoji(text)
+    
+    with open("misc\chatcb_output.txt", "w", encoding='utf8') as file1:
+        file1.write(text)
